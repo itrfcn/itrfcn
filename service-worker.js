74e61e436e9d670ee1b039742dc1dc85c004dcb0
LynxFrost <admin@itrf.cn>
2026-09-05 01:21:17 +0800

    Site updated:2026-09-05 01:21:16

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    Promise.all([
+      caches.keys().then(cacheNames => Promise.all(cacheNames.map(cacheName => caches.delete(cacheName)))),
+      self.registration.unregister()
+    ]).then(() => self.clients.claim())
+  );
+});
